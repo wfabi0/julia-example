4c8c291e373673e89bad893fddfc70b311338625
Fábio Milagres <fabioemanuellmilagres@gmail.com>
2025-10-18 12:52:08 -0300

    atualizado arquivo de structs
    Signed-off-by: Fábio Milagres <fabioemanuellmilagres@gmail.com>

--- 05-structs.jl
+++ 05-structs.jl
@@ -6,3 +6,31 @@ end
 p = Pessoa("Alice", 30)
 
 println(p)
+
+struct Livro
+    titulo::String
+    autor::String
+    ano::Int
+end
+
+struct Biblioteca
+    nome::String
+    livros::Vector{Livro}
+end
+
+livro1 = Livro("1984", "George Orwell", 1949)
+livro2 = Livro("To Kill a Mockingbird", "Harper Lee", 1960)
+
+biblioteca = Biblioteca("Biblioteca Central", [livro1, livro2])
+
+println(biblioteca)
+
+struct Forma
+    cor::String
+    tipo::String
+end
+
+forma1 = Forma("vermelho", "círculo")
+forma2 = Forma("azul", "quadrado")
+
+println(forma1)
